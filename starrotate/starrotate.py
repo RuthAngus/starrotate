6415ad7f501366eade67f64a352ecb991f588140
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2019-03-05 10:56:56 -0800

    initialization print

--- starrotate/starrotate.py
+++ starrotate/starrotate.py
@@ -3,12 +3,13 @@ A script for measuring the rotation periods of a set of stars.
 """
 
 import numpy as np
-from .rotation_tools import simple_acf
+from .rotation_tools import simple_acf, butter_bandpass_filter
 import exoplanet as xo
 import pymc3 as pm
 import theano.tensor as tt
 import matplotlib.pyplot as plt
 import pandas as pd
+import astropy.stats as ass
 
 plotpar = {'axes.labelsize': 25,
            'xtick.labelsize': 20,
@@ -18,67 +19,96 @@ plt.rcParams.update(plotpar)
 
 
 class RotationModel(object):
+    """
+    Code for measuring stellar rotation periods.
 
-    def __init__(self, time, flux, flux_err, t0=None,
-                 duration=None, porb=None):
+    Args:
+        time (array): The time array in days.
+        flux (array): The flux array.
+        flux_err (array): The array of flux uncertainties.
+    """
 
+    def __init__(self, time, flux, flux_err):
         self.time = time
         self.flux = flux
         self.flux_err = flux_err
-        self.t0, self.duration, self.porb = t0, duration, porb
-
-    def plot_lc_zoom(self, zoom=100):
-        plt.figure(figsize=(20, 5))
-        plt.plot(self.raw_time, self.raw_flux, ".", color="C1", ms=2,
-                    zorder=0);
-        plt.xlabel("Time [days]")
-        plt.ylabel("Relative Flux");
-        plt.xlim(self.time[0], self.time[0] + zoom)
-        plt.subplots_adjust(bottom=.15)
+        # self.LS_rotation()
+        # self.ACF_rotation()
 
     def plot_lc(self):
-        # Plot the light curve
+        """
+        Plot the light curve.
+        """
         plt.figure(figsize=(20, 5))
-        plt.plot(self.raw_time, self.raw_flux, "k.", ms=.5);
+        plt.plot(self.time, self.flux, "k.", ms=.5);
         plt.xlabel("Time [days]")
         plt.ylabel("Relative Flux");
         plt.subplots_adjust(bottom=.2)
 
-    def plot_transit_mask(self):
-        ms = 5
-        plt.figure(figsize=(20, 5))
-        plt.plot(self.raw_time, self.raw_flux, ".", color="C1", ms=ms,
-                    zorder=0);
-        plt.plot(self.time, self.flux, ".", color="C0", ms=ms, zorder=1);
-        plt.xlabel("Time [days]")
-        plt.ylabel("Relative Flux");
-        plt.subplots_adjust(bottom=.2)
+    def LS_rotation(self, filter_period=None, order=3):
+                    # min_period=1., max_period=30., samples_per_peak=50):
+        """
+        Measure a rotation period using a Lomb-Scargle periodogram.
 
-    def transit_mask(self, _t0, _duration, porb):
-        t0 = float(_t0) % porb
-        duration = float(_duration) / 24.
+        Args:
+            filter_period (Optional[float]): The minimum period for a high
+                pass filter. Signals with periods longer than this will be
+                removed. For Kepler it's reasonable to set this to 35.
+                Default is None.
+            order (Optional[int]): The order of the Butterworth filter.
+                Default is 3.
+            min_period (Optional[float]): The minimum rotation period you'd
+                like to search for. The default is one day since most stars
+                rotate more slowly than this.
+            max_period (Optional[float]): The maximum rotation period you'd
+                like to search for. For Kepler this could be as high as 70
+                days but for K2 it should probably be more like 20-25 days
+                and 10-15 days for TESS.
+            samples_per_peak (Optional[int]): The number of samples per peak.
 
-        m = np.abs((self.raw_time - t0 + 0.5*self.porb) \
-                   % self.porb - 0.5*self.porb) < 1.5*duration
-        return m
+        Returns:
+            ls_period (float): The Lomb-Scargle rotation period.
 
-    def LS_rotation(self):
+        """
 
         assert len(self.flux) == sum(np.isfinite(self.flux)), "Remove NaNs" \
             " from your flux array before trying to compute a periodogram."
 
-        # Calculate a LS period
-        results = xo.estimators.lomb_scargle_estimator(
-            self.time, self.flux, max_peaks=1, min_period=1.0,
-            max_period=30.0, samples_per_peak=50)
+        # # Calculate a LS period
+        # results = xo.estimators.lomb_scargle_estimator(
+        #     self.time, self.flux, max_peaks=1, min_period=min_period,
+        #     max_period=max_period, samples_per_peak=samples_per_peak)
+        # self.freq, self.power = results["periodogram"]
+        # peak = results["peaks"][0]
+        # self.ls_period = peak["period"]
+        # return peak["period"]
 
-        self.freq, self.power = results["periodogram"]
-        peak = results["peaks"][0]
-        self.ls_period = peak["period"]
-        return peak["period"]
+        self.freq = np.linspace(1./100, 1./.1, 100000)
+        ps = 1./self.freq
+
+        if filter_period is not None:
+                fs = 1./(self.time[1] - self.time[0])
+                lowcut = 1./filter_period
+                yfilt = butter_bandpass_filter(self.flux, lowcut, fs,
+                                                  order=3)
+        else:
+            yfilt = self.flux*1
+
+        self.power = ass.LombScargle(
+            self.time, yfilt, self.flux_err).power(self.freq)
+        peaks = np.array([i for i in range(1, len(ps)-1) if self.power[i-1] <
+                          self.power[i] and self.power[i+1] < self.power[i]])
+
+        self.ls_period = ps[self.power == max(self.power[peaks])][0]
+        return self.ls_period
+
 
     def pgram_plot(self):
-        # Make a plot of the periodogram.
+        """
+        Make a plot of the periodogram.
+
+        """
+
         plt.figure(figsize=(16, 9))
         plt.plot(-np.log10(self.freq), self.power, "k", zorder=0)
         plt.axvline(np.log10(self.ls_period), color="C1", lw=4, alpha=0.5,
@@ -89,8 +119,20 @@ class RotationModel(object):
         plt.ylabel("Power");
         plt.subplots_adjust(left=.15, bottom=.15)
 
-    def ACF_rotation(self):
-        lags, acf, acf_period = simple_acf(self.time, self.flux)
+    def ACF_rotation(self, interval):
+        """
+        Calculate a rotation period based on an autocorrelation function.
+
+        Args:
+            interval (float): The time in days between observations. For
+                Kepler/K2 long cadence this is 0.02043365, for Tess its about
+                0.00138889 days.
+
+        Returns:
+            acf_period (float): The ACF rotation period in days.
+        """
+
+        lags, acf, acf_period = simple_acf(self.time, self.flux, interval)
 
         self.lags = lags
         self.acf = acf
@@ -98,6 +140,10 @@ class RotationModel(object):
         return acf_period
 
     def acf_plot(self):
+        """
+        Make a plot of the autocorrelation function.
+
+        """
         plt.figure(figsize=(16, 9))
         plt.plot(self.lags, self.acf, "k")
         plt.axvline(self.acf_period, color="C1")
@@ -106,14 +152,43 @@ class RotationModel(object):
         plt.xlim(0, max(self.lags))
         plt.subplots_adjust(left=.15, bottom=.15)
 
-    def GP_rotation(self, init_period=None):
-        x = self.time
-        y = self.flux
-        yerr = self.flux_err
+    def GP_rotation(self, init_period=None, tune=2000, draws=2000,
+                    prediction=True, cores=None):
+        """
+        Calculate a rotation period using a Gaussian process method.
+
+        Args:
+            init_period (Optional[float]): Your initial guess for the rotation
+                period. The default is the Lomb-Scargle period.
+            tune (Optional[int]): The number of tuning samples. Default is
+                2000.
+            draws (Optional[int]): The number of samples. Default is 2000.
+            prediction (Optional[Bool]): If true, a prediction will be
+                calculated for each sample. This is useful for plotting the
+                prediction but will slow down the whole calculation.
+            cores (Optional[int]): The number of cores to use. Default is
+                None (for running one process).
+
+        Returns:
+            gp_period (float): The GP rotation period in days.
+            errp (float): The upper uncertainty on the rotation period.
+            errm (float): The lower uncertainty on the rotation period.
+            logQ (float): The Q factor.
+            Qerrp (float): The upper uncertainty on the Q factor.
+            Qerrm (float): The lower uncertainty on the Q factor.
+        """
+        self.prediction = prediction
+
+        x = np.array(self.time, dtype=float)
+        # Median of data must be zero
+        y = np.array(self.flux, dtype=float) - np.median(self.flux)
+        yerr = np.array(self.flux_err, dtype=float)
 
         if init_period is None:
             # Calculate ls period
             init_period = self.LS_rotation()
+            print("No initial period provided, initializing with the LS " \
+                  "period, {0:.2f} days.".format(self.ls_period))
 
         with pm.Model() as model:
 
@@ -149,65 +224,76 @@ class RotationModel(object):
             pm.Potential("loglike", gp.log_likelihood(y - mean))
 
             # Compute the mean model prediction for plotting purposes
-            # pm.Deterministic("pred", gp.predict())
+            if prediction:
+                pm.Deterministic("pred", gp.predict())
 
             # Optimize to find the maximum a posteriori parameters
             self.map_soln = pm.find_MAP(start=model.test_point)
-
-            # Plot the MAP fit.
-            # if self.plot:
-            #     plt.figure(figsize=(20, 5))
-            #     # plt.plot(x, y, "k.", ms=.5, label="$\mathrm{data}$")
-            #     # plt.plot(x, map_soln["pred"], color="C1", lw=3,
-            #     #         label="$\mathrm{model}$")
-            #     # plt.xlabel("$\mathrm{Time~[days]}$")
-            #     # plt.ylabel("$\mathrm{Relative~flux}$")
-            #     plt.plot(x, y, "k.", ms=.5, label="data")
-            #     plt.plot(x, map_soln["pred"], color="C1", lw=3,
-            #             label="model")
-            #     plt.xlabel("Time [days]")
-            #     plt.ylabel("Relative flux")
-            #     plt.legend(fontsize=20)
 
             # Sample from the posterior
             np.random.seed(42)
             sampler = xo.PyMC3Sampler()
             with model:
-                sampler.tune(tune=2000, start=map_soln,
-                            step_kwargs=dict(target_accept=0.9), cores=1)
-                trace = sampler.sample(draws=2000, cores=1)
+                print("sampling...")
+                sampler.tune(tune=tune, start=self.map_soln,
+                            step_kwargs=dict(target_accept=0.9), cores=cores)
+                trace = sampler.sample(draws=draws, cores=cores)
 
             # Save samples
             samples = pm.trace_to_dataframe(trace)
             self.samples = samples
 
             self.period_samples = trace["period"]
-            gp_period = np.median(self.period_samples)
+            self.gp_period = np.median(self.period_samples)
             lower = np.percentile(self.period_samples, 16)
             upper = np.percentile(self.period_samples, 84)
-            errm = gp_period - lower
-            errp = upper - gp_period
-            logQ = np.median(trace["logQ0"])
+            self.errm = self.gp_period - lower
+            self.errp = upper - self.gp_period
+            self.logQ = np.median(trace["logQ0"])
             upperQ = np.percentile(trace["logQ0"], 84)
             lowerQ = np.percentile(trace["logQ0"], 16)
-            Qerrp = upperQ - logQ
-            Qerrm = logQ - lowerQ
+            self.Qerrp = upperQ - self.logQ
+            self.Qerrm = self.logQ - lowerQ
 
-        self.gp_period = gp_period
-        self.errp, self.errm = errp, errm
-        self.logQ = logQ
-        self.Qerrp, self.Qerrm = Qerrp, Qerrm
+        self.trace = trace
 
-        return gp_period, errp, errm, logQ, Qerrp, Qerrm
+        return self.gp_period, self.errp, self.errm, self.logQ, self.Qerrp, \
+            self.Qerrm
 
-    def plot_posterior(self):
+    def plot_prediction(self):
+        """
+        Plot the GP prediction, fit to the data.
 
-        # Plot the posterior.
-        plt.hist(self.period_samples, 30, histtype="step", color="k")
+        """
+        if not self.prediction:
+            print("You must run GP_rotate with prediction=True in order" \
+                    " to plot the prediction.")
+            return
+
+        plt.figure(figsize=(20, 5))
+        plt.plot(self.time, self.flux-np.median(self.flux), "k.", ms=2,
+                 label="data")
+        plt.plot(self.time, np.median(self.trace["pred"], axis=0),
+                    color="C1", lw=2, label="model")
+        plt.xlabel("Time [days]")
+        plt.ylabel("Relative flux")
+        plt.legend(fontsize=20)
+        self.prediction = np.median(self.trace["pred"], axis=0)
+
+    def plot_posterior(self, nbins=30, cutoff=50.):
+        """
+        Plot the posterior probability density function for rotation period.
+
+        Args:
+            nbins (Optional[int]): The number of histogram bins. Default is 30
+            cutoff (Optional[float]): The maximum sample value to plot.
+        """
+        samps = self.period_samples[self.period_samples < cutoff]
+        plt.hist(self.period_samples, nbins, histtype="step", color="k")
         plt.axvline(self.gp_period)
         plt.yticks([])
         plt.xlabel("Rotation period [days]")
         plt.ylabel("Posterior density");
-        plt.axvline(lower, ls="--", color="C1");
-        plt.axvline(upper, ls="--", color="C1");
-        plt.xlim(0, 50);
+        plt.axvline(self.gp_period - self.errm, ls="--", color="C1");
+        plt.axvline(self.gp_period + self.errp, ls="--", color="C1");
+        plt.xlim(0, cutoff);
